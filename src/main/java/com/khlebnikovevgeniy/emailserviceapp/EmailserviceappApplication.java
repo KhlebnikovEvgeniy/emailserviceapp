cfbb7351a64b33949969826f5668fcacd7668e1c
KhlebnikovEvgeniy <khebnikove@gmail.com>
2025-05-03 19:30:37 +0300

    Initial Commit

---
+++ src/main/java/com/khlebnikovevgeniy/emailserviceapp/EmailserviceappApplication.java
@@ -0,0 +1,13 @@
+package com.khlebnikovevgeniy.emailserviceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailserviceappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailserviceappApplication.class, args);
+	}
+
+}
