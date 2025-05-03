8f2e9caa5bcf921cda02ec71f44152893c9fe0bf
KhlebnikovEvgeniy <khebnikove@gmail.com>
2025-05-03 21:50:02 +0300

    User Resource - 2

--- src/main/java/com/khlebnikovevgeniy/emailserviceapp/resource/UserResource.java
+++ src/main/java/com/khlebnikovevgeniy/emailserviceapp/resource/UserResource.java
@@ -6,9 +6,11 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.khlebnikovevgeniy.emailserviceapp.domain.HttpResponse;
@@ -33,6 +35,20 @@ public class UserResource {
 										.message("User created")
 										.httpStatus(HttpStatus.CREATED)
 										.statusCode(HttpStatus.CREATED.value())
+										.build()
+		);
+	}
+	
+	@GetMapping
+	public ResponseEntity<HttpResponse> confirmUserAccount(@RequestParam("token") String token) {
+		Boolean isSuccess = userService.verifyToken(token);
+		return ResponseEntity.ok().body(
+							HttpResponse.builder()
+										.timeStamp(LocalDateTime.now().toString())
+										.data(Map.of("success", isSuccess))
+										.message("Account verified")
+										.httpStatus(HttpStatus.OK)
+										.statusCode(HttpStatus.OK.value())
 										.build()
 		);
 	}
