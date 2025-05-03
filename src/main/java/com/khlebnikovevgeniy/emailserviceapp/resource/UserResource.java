b5365a1c43fe3d5d37d70ddd29ee0770ee7b7f9c
KhlebnikovEvgeniy <khebnikove@gmail.com>
2025-05-03 21:38:46 +0300

    User Resource - 1

---
+++ src/main/java/com/khlebnikovevgeniy/emailserviceapp/resource/UserResource.java
@@ -0,0 +1,40 @@
+package com.khlebnikovevgeniy.emailserviceapp.resource;
+
+import java.net.URI;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.khlebnikovevgeniy.emailserviceapp.domain.HttpResponse;
+import com.khlebnikovevgeniy.emailserviceapp.domain.User;
+import com.khlebnikovevgeniy.emailserviceapp.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserResource {
+	private final UserService userService;
+	
+	@PostMapping
+	public ResponseEntity<HttpResponse> createUser(@RequestBody User user) {
+		User newUser = userService.saveUser(user);
+		return ResponseEntity.created(URI.create("")).body(
+							HttpResponse.builder()
+										.timeStamp(LocalDateTime.now().toString())
+										.data(Map.of("user", newUser))
+										.message("User created")
+										.httpStatus(HttpStatus.CREATED)
+										.statusCode(HttpStatus.CREATED.value())
+										.build()
+		);
+	}
+	
+}
